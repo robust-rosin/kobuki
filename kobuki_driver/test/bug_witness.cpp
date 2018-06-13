779097ef8cca09b0ed6b6c4048e7615900b179a5
git-afsantos <contact.andre.santos@gmail.com>
2018-06-13 15:26:25 +0100

    added unit test

---
+++ kobuki_driver/test/bug_witness.cpp
@@ -0,0 +1,21 @@
+#include <climits>
+#include "kobuki_driver/modules/diff_drive.hpp"
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+
+TEST(TestSuite, bugWitness)
+{
+  const double cmd_speed = 10.0;
+  const double cmd_radius = -0.0872665;
+  kobuki::DiffDrive drive;
+  drive.velocityCommands(cmd_speed, cmd_radius);
+  std::vector<short> commands = drive.velocityCommands();
+  EXPECT_EQ((short)SHRT_MAX, commands[0]);
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "tester");
+  ros::NodeHandle nh;
+  return RUN_ALL_TESTS();
+}
